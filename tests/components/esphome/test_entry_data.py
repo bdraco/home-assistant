ca56d304cda3478d4c069be77e8044418d166d9c
J. Nick Koston <nick@koston.org>
2023-10-15 15:42:18 -1000

    Apply suggestions from code review

--- tests/components/esphome/test_entry_data.py
+++ tests/components/esphome/test_entry_data.py
@@ -47,12 +47,11 @@ async def test_migrate_entity_unique_id(
     state = hass.states.get("sensor.old_sensor")
     assert state is not None
     assert state.state == "50"
-    assert state.attributes[ATTR_ICON] == "mdi:leaf"
     entity_reg = er.async_get(hass)
     entry = entity_reg.async_get("sensor.old_sensor")
     assert entry is not None
     assert entity_reg.async_get_entity_id("sensor", "esphome", "my_sensor") is None
-    # Not that ESPHome includes the EntityInfo type in the unique id
+    # Note that ESPHome includes the EntityInfo type in the unique id
     # as this is not a 1:1 mapping to the entity platform (ie. text_sensor)
     assert entry.unique_id == "11:22:33:44:55:aa-sensor-mysensor"
 
@@ -99,7 +98,6 @@ async def test_migrate_entity_unique_id_downgrade_upgrade(
     state = hass.states.get("sensor.new_sensor")
     assert state is not None
     assert state.state == "50"
-    assert state.attributes[ATTR_ICON] == "mdi:leaf"
     entity_reg = er.async_get(hass)
     entry = entity_reg.async_get("sensor.new_sensor")
     assert entry is not None
