561014fc6f480469dda0a1af714128a0dd09cc81
J. Nick Koston <nick@koston.org>
2023-10-17 13:07:53 -1000

    test for single char and service removal

--- tests/components/homekit_controller/specific_devices/test_ecobee3.py
+++ tests/components/homekit_controller/specific_devices/test_ecobee3.py
@@ -305,10 +305,10 @@ async def test_ecobee3_remove_sensors_at_runtime(hass: HomeAssistant) -> None:
     # the same entity from being added twice.
 
 
-async def test_ecobee3_remove_on_device_temp_sensor_at_run_time(
+async def test_ecobee3_services_and_chars_removed(
     hass: HomeAssistant,
 ) -> None:
-    """Test that a service is removed from the accessory but not the accessory itself."""
+    """Test handling removal of some services and chars."""
     entity_registry = er.async_get(hass)
 
     # Set up a base Ecobee 3 with additional sensors.
@@ -318,14 +318,25 @@ async def test_ecobee3_remove_on_device_temp_sensor_at_run_time(
     climate = entity_registry.async_get("climate.homew")
     assert climate.unique_id == "00:00:00:00:00:00_1_16"
 
-    temp_sensor = entity_registry.async_get("sensor.homew_current_temperature")
-    assert temp_sensor.unique_id == "00:00:00:00:00:00_1_16_19"
+    assert hass.states.get("sensor.basement_temperature") is not None
+    assert hass.states.get("sensor.kitchen_temperature") is not None
+    assert hass.states.get("sensor.porch_temperature") is not None
 
-    # Reconfigure with the temperature service removed
+    assert hass.states.get("select.homew_current_mode") is not None
+    assert hass.states.get("button.homew_clear_hold") is not None
+
+    # Reconfigure with some of the chars removed and the basement temperature sensor
     accessories = await setup_accessories_from_file(
         hass, "ecobee3_service_removed.json"
     )
     await device_config_changed(hass, accessories)
 
+    # Make sure the climate entity is still there
     assert hass.states.get("climate.homew") is not None
-    assert hass.states.get("sensor.homew_current_temperature") is None
+
+    # Make sure the basement temperature sensor is gone
+    assert hass.states.get("sensor.basement_temperature") is None
+
+    # Make sure the current mode select and clear hold button are gone
+    assert hass.states.get("select.homew_current_mode") is None
+    assert hass.states.get("button.homew_clear_hold") is None
