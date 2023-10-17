3a4744fd6d18e027e6286e220da7aeb85d649e70
J. Nick Koston <nick@koston.org>
2023-10-17 12:14:52 -1000

    add reverse tests for ecobee

--- tests/components/homekit_controller/specific_devices/test_ecobee3.py
+++ tests/components/homekit_controller/specific_devices/test_ecobee3.py
@@ -252,3 +252,37 @@ async def test_ecobee3_add_sensors_at_runtime(hass: HomeAssistant) -> None:
 
     occ3 = entity_registry.async_get("binary_sensor.basement")
     assert occ3.unique_id == "00:00:00:00:00:00_4_56"
+
+
+async def test_ecobee3_remove_sensors_at_runtime(hass: HomeAssistant) -> None:
+    """Test that sensors are automatically remove."""
+    entity_registry = er.async_get(hass)
+
+    # Set up a base Ecobee 3 with additional sensors.
+    accessories = await setup_accessories_from_file(hass, "ecobee3.json")
+    await setup_test_accessories(hass, accessories)
+
+    climate = entity_registry.async_get("climate.homew")
+    assert climate.unique_id == "00:00:00:00:00:00_1_16"
+
+    occ1 = entity_registry.async_get("binary_sensor.kitchen")
+    assert occ1.unique_id == "00:00:00:00:00:00_2_56"
+
+    occ2 = entity_registry.async_get("binary_sensor.porch")
+    assert occ2.unique_id == "00:00:00:00:00:00_3_56"
+
+    occ3 = entity_registry.async_get("binary_sensor.basement")
+    assert occ3.unique_id == "00:00:00:00:00:00_4_56"
+
+    assert hass.states.get("binary_sensor.kitchen") is not None
+    assert hass.states.get("binary_sensor.porch") is not None
+    assert hass.states.get("binary_sensor.basement") is not None
+
+    # Now remove 3 new sensors at runtime - sensors should disappear and climate
+    # shouldn't be duplicated.
+    accessories = await setup_accessories_from_file(hass, "ecobee3_no_sensors.json")
+    await device_config_changed(hass, accessories)
+
+    assert hass.states.get("binary_sensor.kitchen") is None
+    assert hass.states.get("binary_sensor.porch") is None
+    assert hass.states.get("binary_sensor.basement") is None
