111a98a99e2a4f8806f442a17c042c5ef16ae0ef
J. Nick Koston <nick@koston.org>
2023-10-17 15:45:31 -1000

    Update for service data

--- homeassistant/components/improv_ble/config_flow.py
+++ homeassistant/components/improv_ble/config_flow.py
@@ -128,6 +128,24 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the Bluetooth discovery step."""
         await self.async_set_unique_id(discovery_info.address)
         self._abort_if_unique_id_configured()
+        service_data = discovery_info.service_data
+        # TODO: make uuid a constant from the library
+        improv_data = service_data.get("00004677-0000-1000-8000-00805f9b34fb")
+        # TODO: move data layout comments to library
+        # improv_data layout:
+        #   0: state
+        #   1: capabilities
+        #   2: reserved
+        #   3: reserved
+        #   4: reserved
+        #   5: reserved
+        if not improv_data:
+            # TODO: test case for this
+            return self.async_abort(reason="not_improv_device")
+        improv_state = improv_data[0]
+        if improv_state in (State.PROVISIONING, State.PROVISIONED):
+            # TODO: test case for this
+            return self.async_abort(reason="already_provisioned")
         self._discovery_info = discovery_info
         name = self._discovery_info.name or self._discovery_info.address
         self.context["title_placeholders"] = {"name": name}
