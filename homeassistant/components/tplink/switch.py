sdb9696/core

Add authentication flows to tplink integration to enable newer device protocol support

homeassistant/components/tplink/switch.py
@@ -39,7 +39,8 @@ async def async_setup_entry(
     elif device.is_plug:
         entities.append(SmartPlugSwitch(device, coordinator))
 
-    entities.append(SmartPlugLedSwitch(device, coordinator))
+    if hasattr(device, "led"):
+        entities.append(SmartPlugLedSwitch(device, coordinator))
 
     async_add_entities(entities)
 

bdraco/home-assistant

homeassistant/components/tplink/switch.py
@@ -41,7 +41,8 @@ async def async_setup_entry(
     elif device.is_plug:
         entities.append(SmartPlugSwitch(device, parent_coordinator))
 
-    entities.append(SmartPlugLedSwitch(device, parent_coordinator))
+    if hasattr(device, "led"):
+        entities.append(SmartPlugLedSwitch(device, parent_coordinator))
 
     async_add_entities(entities)
 