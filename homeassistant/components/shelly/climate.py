b63b28fd2c2682f76753acff24cb7787a105d0fd
Simone Chemelli <simone.chemelli@gmail.com>
2024-02-05 18:59:04 +0000

    improve coverage and clean switch code

--- homeassistant/components/shelly/climate.py
+++ homeassistant/components/shelly/climate.py
@@ -43,7 +43,12 @@ from .const import (
 )
 from .coordinator import ShellyBlockCoordinator, ShellyRpcCoordinator, get_entry_data
 from .entity import ShellyRpcEntity
-from .utils import async_remove_shelly_entity, get_device_entry_gen, get_rpc_key_ids
+from .utils import (
+    async_remove_shelly_entity,
+    get_device_entry_gen,
+    get_rpc_key_ids,
+    is_rpc_thermostat_internal_actuator,
+)
 
 
 async def async_setup_entry(
@@ -125,17 +130,12 @@ def async_setup_rpc_entry(
 
     climate_ids = []
     for id_ in climate_key_ids:
-        if thermostat := coordinator.device.config.get(f"thermostat:{id_}"):
-            mac: str = coordinator.device.config["sys"]["device"]["mac"]
-
-            if not thermostat["actuator"].startswith(
-                f"shelly://shellywalldisplay-{mac.lower()}"
-            ):
-                # Wall Display relay is used as the external switch actuator,
-                # we need to remove the climate entity
-                unique_id = f"{coordinator.mac}-thermostat:{id_}"
-                async_remove_shelly_entity(hass, "climate", unique_id)
-                continue
+        if not is_rpc_thermostat_internal_actuator(coordinator.device.config, id_):
+            # Wall Display relay is used as the external switch actuator,
+            # we need to remove the climate entity
+            unique_id = f"{coordinator.mac}-thermostat:{id_}"
+            async_remove_shelly_entity(hass, "climate", unique_id)
+            continue
 
         climate_ids.append(id_)
 
