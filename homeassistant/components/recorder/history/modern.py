32b53612360e2730e202b7be5f9a072b0512a158
J. Nick Koston <nick@koston.org>
2024-12-19 01:21:23 -1000

    Revert "Additional optimizations to the start time queries"
    This reverts commit bde0df3612efa107aca96ab694c03f0b116c506c.

--- homeassistant/components/recorder/history/modern.py
+++ homeassistant/components/recorder/history/modern.py
@@ -577,19 +577,22 @@ def _get_start_time_state_for_entities_stmt(
         .select_from(StatesMeta)
         .join(
             States,
-            States.state_id
-            == (
-                select(States.state_id)
-                .where(
-                    (StatesMeta.metadata_id == States.metadata_id)
-                    & (States.last_updated_ts < epoch_time)
-                    & (States.last_updated_ts >= run_start_ts)
+            and_(
+                States.last_updated_ts
+                == (
+                    select(States.last_updated_ts)
+                    .where(
+                        (StatesMeta.metadata_id == States.metadata_id)
+                        & (States.last_updated_ts < epoch_time)
+                        & (States.last_updated_ts >= run_start_ts)
+                    )
+                    .order_by(States.last_updated_ts.desc())
+                    .limit(1)
                 )
-                .order_by(States.last_updated_ts.desc())
-                .limit(1)
-            )
-            .scalar_subquery()
-            .correlate(StatesMeta),
+                .scalar_subquery()
+                .correlate(StatesMeta),
+                States.metadata_id == StatesMeta.metadata_id,
+            ),
         )
         .where(StatesMeta.metadata_id.in_(metadata_ids))
     )
